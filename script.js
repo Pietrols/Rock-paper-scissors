6fd108f27bb66c2c1cd7c306d22e6a9d09ee2c4e
Peter Kabamba <kabambapeter3@gmail.com>
2024-12-19 14:55:02 +0200

    Second commit, attemping to write and test code for steps 1 to 4. in order to have the functions perform desired task.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //write a function that returns rock, paper or scissors randomly using Math.random
+let computerChoice;
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
-  let computerChoice;
-
   if (randomNumber === 0) {
     computerChoice = "Rock";
   } else if (randomNumber === 1) {
@@ -14,8 +13,36 @@ function getComputerChoice() {
 }
 
 //write a function that takes human choice and returns it.
-function getHumanChoice() {}
+function getHumanChoice() {
+  const humanChoice = prompt("Rock, Paper or Scissors?");
+  return playRound;
+}
 
 //write a variable to keep player scores.
+const humanScore = 0;
+const computerScore = 0;
+
 //write the logic to play a single round.
+function playRound(humanChoice, computerChoice) {
+  const draw = prompt("I'ts a tie!");
+  const lose = prompt("You Lose! ${humanChoice} beats ${computerChoice}");
+  const win = prompt("You win! ${computerChoice} beats ${humanChoice}");
+
+  if (humanChoice.toLowerCase() === computerChoice) {
+    return draw;
+  } else if (
+    (humanChoice.toLowerCase() === "Rock" && computerChoice === "Scissors") ||
+    (humanChoice.toLowerCase() === "Scissors" && computerChoice === "Paper") ||
+    (humanChoice.toLowerCase() === "Paper" && computerChoice === "Rock")
+  ) {
+    return win;
+  } else {
+    return lose;
+  }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
 //write the logic to play entire game
